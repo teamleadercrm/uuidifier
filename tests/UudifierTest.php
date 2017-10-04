9b11dda31d52fe8d02c0001a7c0ffcd941dbb0d0
woutersioen <wouter.sioen@gmail.com>
2017-10-04 10:29:38 +0200

    Add a failing test proving 2 different uuid's can have the same integer

--- tests/UudifierTest.php
+++ tests/UudifierTest.php
@@ -3,6 +3,7 @@
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\UuidInterface;
 use Teamleader\Uuidifier\Uuidifier;
+use Ramsey\Uuid\Uuid;
 
 class UudifierTest extends TestCase
 {
@@ -49,5 +50,24 @@ class UudifierTest extends TestCase
         $uuid1 = $generator->encode('foo', 1);
         $uuid2 = $generator->encode('bar', 1);
         $this->assertNotEquals($uuid1->toString(), $uuid2->toString());
+    }
+
+    /**
+     * @test
+     * This test proves that I can decode 2 different uuid strings to the same integer
+     */
+    public function itDecodesUniquely()
+    {
+        $generator = new Uuidifier();
+        $uuid1 = $generator->encode('foo', 1);
+        $uuid2 = Uuid::fromString(rtrim($uuid1, '1') . '2');
+
+        $realUuid2 = $generator->encode('foo', 2);
+        $this->assertNotEquals($realUuid2, $uuid1);
+
+        $decodedUuid2 = $generator->decode('foo', $uuid2);
+        $decodedRealUuid2 = $generator->decode('foo', $realUuid2);
+
+        $this->assertNotEquals($decodedUuid2, $decodedRealUuid2);
     }
 }
