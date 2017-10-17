ba4c37e1ce1c447d2dff1413b8ac6fdf9201cd9a
Jonas Drieghe <jonasd@madewithlove.be>
2017-10-17 09:18:06 +0200

    Add simple console command to encode uuids.

---
+++ src/Command/Encode.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Teamleader\Uuidifier\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Teamleader\Uuidifier\Uuidifier;
+
+class Encode extends Command
+{
+    protected function configure()
+    {
+        $this->setName('uuidifier:encode');
+        $this->setDescription('Encode a prefix and int to a uuid string');
+        $this->addArgument(
+            'prefix',
+            InputArgument::REQUIRED,
+            'The prefix, often the type of the identifier you want to encode'
+        );
+        $this->addArgument(
+            'id',
+            InputArgument::REQUIRED,
+            'The integer identifier you want to encode'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $uuidifier = new UuidIfier();
+        $prefix = $input->getArgument('prefix');
+        $id = (int) $input->getArgument('id');
+        $uuid = $uuidifier->encode(
+            $prefix,
+            $id
+        );
+        $output->writeln(
+            "Id $id with prefix $prefix encodes as $uuid"
+        );
+    }
+}
