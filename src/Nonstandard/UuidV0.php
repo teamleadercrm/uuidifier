51f6ca761c2d0d64b589ec1a80fc03b8ceaecae3
Stephen Beirlaen <stephen@beirlaen.be>
2022-05-12 21:06:27 +0200

    Fix decoding UuidV0's

--- src/Nonstandard/UuidV0.php
+++ src/Nonstandard/UuidV0.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace Teamleader\Uuidifier\Nonstandard;
 
 use Ramsey\Uuid\Codec\CodecInterface;
+use Ramsey\Uuid\Codec\StringCodec;
+use Ramsey\Uuid\Converter\Number\GenericNumberConverter;
 use Ramsey\Uuid\Converter\NumberConverterInterface;
+use Ramsey\Uuid\Converter\Time\GenericTimeConverter;
 use Ramsey\Uuid\Converter\TimeConverterInterface;
 use Ramsey\Uuid\Exception\InvalidArgumentException;
+use Ramsey\Uuid\Math\BrickMathCalculator;
 use Ramsey\Uuid\Rfc4122\FieldsInterface as Rfc4122FieldsInterface;
 use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidFactory;
+use Ramsey\Uuid\UuidFactoryInterface;
+use Ramsey\Uuid\UuidInterface;
+use Teamleader\Uuidifier\Builder\VersionZeroUuidBuilder;
 
 final class UuidV0 extends Uuid
 {
@@ -26,5 +34,34 @@ final class UuidV0 extends Uuid
         }
 
         parent::__construct($fields, $numberConverter, $codec, $timeConverter);
+    }
+
+    /**
+     * Override this method to avoid returning a LazyUuidFromString
+     */
+    public static function fromString(string $uuid): UuidInterface
+    {
+        $uuid = strtolower($uuid);
+
+        return self::getFactory()->fromString($uuid);
+    }
+
+    /**
+     * Override this method to avoid returning a Ramsey\Uuid\UuidFactory
+     */
+    public static function getFactory(): UuidFactoryInterface
+    {
+        $uuidFactory = new UuidFactory();
+
+        $calculator = new BrickMathCalculator();
+        $versionZeroUuidBuilder = new VersionZeroUuidBuilder(
+            new GenericNumberConverter($calculator),
+            new GenericTimeConverter($calculator),
+        );
+
+        $uuidFactory->setCodec(new StringCodec($versionZeroUuidBuilder));
+        $uuidFactory->setUuidBuilder($versionZeroUuidBuilder);
+
+        return $uuidFactory;
     }
 }
